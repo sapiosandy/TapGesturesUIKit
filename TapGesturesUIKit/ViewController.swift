87a1378e51015a6e99bff351e3c7d0cb7e4a6b87
Sandy Gomez <sgomez1029@gmail.com>
2024-03-04 18:37:27 -0800

    Initial Commit

---
+++ TapGesturesUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TapGesturesUIKit
+//
+//  Created by Sandra Gomez on 3/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
