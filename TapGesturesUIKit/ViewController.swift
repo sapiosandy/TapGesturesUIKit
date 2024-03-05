8612759b4121cca384c9ad0851e45e29af38fe4e
Sandy Gomez <sgomez1029@gmail.com>
2024-03-04 18:46:30 -0800

    Tap Gesture Recognizer

--- TapGesturesUIKit/ViewController.swift
+++ TapGesturesUIKit/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func oneTapGesture(_ sender: UITapGestureRecognizer) {
+        myView.backgroundColor = UIColor.red
+    }
+    @IBAction func twoTapGesture(_ sender: UITapGestureRecognizer) {
+        myView.backgroundColor = UIColor.yellow
+    }
+    
 }
 
